b77ad1e6ee50badbd26ae1d2d90e95cb96711caf
deaboway <deaboway@gmail.com>
2011-08-13 22:56:13 +0800

    initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gademo::Application.config.secret_token = '12e091bfad24a7c43ee90f3822e71f90827dc13d8e4460c972a7f3073827ccaf551ae5a7f05e7f9dbae98e36fb69ef2a575285a4dbbab8303ec821ce5d6dc8b0'
